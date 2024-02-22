eda4a3399aed1d684757cb922e15db2cfe66c33d
Syed Hammad Ullah <syedhammadullah660@gmail.com>
2024-02-22 07:05:23 +0500

    Learning Javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+// Variable
+let Myname = "Syed Hammad Ullah";
+let Myage = 18;
+let a = (parseInt = prompt("1 value"));
+let b = (parseInt = prompt("2 value"));
+let c = a + b;
+
+console.log(Myname);
+console.log(Myname);
+console.log(Myname);
+console.log(Myname);
+console.log(Myname);
+
+console.log(Myage);
+console.log(Myage);
+console.log(Myage);
+console.log(Myage);
+console.log(Myage);
+
+console.log(Myage+2);
+
+const pi = 3.142;
+const g = 9.8;
+
+
